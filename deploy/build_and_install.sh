e95159a552c162b9373257306e0759af24d35e16
shaobo.chen <shaobo.chen@chaitin.com>
2021-04-29 14:11:26 +0800

    init project

---
+++ deploy/build_and_install.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+
+echo "LOCALHOST TIME: "$(date -R)
+start=$(date +%s)
+
+command -v docker > /dev/null && echo "docker found" || { echo "docker not found"; exit 1; }
+command -v docker-compose > /dev/null && echo "docker-compose found" || { echo "docker-compose not found"; exit 1; }
+
+echo "------------clean remain env------------"
+if [[ $(docker ps | grep clt_) != "" ]]; then
+  docker-compose -p message_guest -f docker-compose.yml down
+fi
+if [[ $(docker images | grep clt_ | awk '{print $1}') != "" ]]; then
+  docker image rm $(docker images | grep clt_ | awk '{print $1}')
+fi
+
+echo "------------build the web------------"
+docker run -it --rm -v $(pwd)/../web:/web node:15.4.0-stretch bash -c "cd /web && yarn install && yarn versions && yarn build || cat /root/.npm/_logs/*.log" || { echo "web build fail"; exit 1; }
+
+echo "------------copy the dist to nginx------------"
+cp -r ../web/dist ../nginx
+
+echo "------------build nginx------------"
+docker build -t clt_nginx ../nginx
+
+echo "------------build server------------"
+docker build -t clt_server ../server
+
+export HTTP_PORT='8000'
+export MONGODB_ADDR='mongodb://clt_mongo:27017/guest_book'
+export REDIS_ADDR='redis://clt_redis:6379/0'
+docker-compose -p message_guest -f docker-compose.yml up -d
+
+end=$(date +%s)
+echo "TOTAL SPEND TIME: "$(expr $end - $start)" seconds"
---
+++ nginx/ssl_cert.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+certbot --nginx -d shaobo.fun
+#certbot renew
